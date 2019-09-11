35c328da50547acacaed0804806e990479b5f45c
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-09-11 10:54:41 -0300

    Create global style

---
+++ src/styles/global.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    box-sizing: border-box;
+    margin: 0px;
+    outline: 0px;
+    padding: 0px;
+  }
+
+  *:focus {
+    outline: 0px;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
