2e543136451bf1304284a564d814128b3c1250e7
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-24 20:31:08 -0300

    Update rewired config

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,9 @@
-const { addBabelPlugin, override } = require('customize-cra');
+const jestConfig = require('./jest.config');
 
-module.exports = override(
-  addBabelPlugin([
-    'babel-plugin-root-import',
-    {
-      rootPathSuffix: 'src',
-    },
-  ])
-);
+module.exports = {
+  jest(config) {
+    config.preset = jestConfig.preset;
+    config.reporters = jestConfig.reporters;
+    return config;
+  },
+};
