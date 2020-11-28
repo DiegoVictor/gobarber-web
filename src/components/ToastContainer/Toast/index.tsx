6b893853489ff39d914345fb67a9ad5b9856e03d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-11-28 00:26:22 -0300

    Create toast component

---
+++ src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
+
+import { ToastMessage, useToast } from '../../../hooks/toast';
+import { Container } from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+  style: { [key: string]: string | number | undefined };
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, removeToast]);
+
+  return (
+    <Container
+      style={style}
+      $hasDescription={Number(!!message.description)}
+      type={message.type}
+    >
+      {icons[message.type || 'info']}
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button
+        onClick={() => removeToast(message.id)}
+        type="button"
+        data-testid="remove"
+      >
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
---
+++ src/components/ToastContainer/Toast/styles.ts
@@ -0,0 +1,72 @@
+import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
+
+interface ContainerProps {
+  type?: 'success' | 'error' | 'info';
+  $hasDescription: number;
+}
+
+const ToastTypeVariations = {
+  info: css`
+    background-color: #ebf8ff;
+    color: #3172b7;
+  `,
+  success: css`
+    background-color: #e6fffa;
+    color: #2e656a;
+  `,
+  error: css`
+    background-color: #fddede;
+    color: #c53030;
+  `,
+};
+
+export const Container = styled(animated.div)<ContainerProps>`
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+  display: flex;
+  padding: 16px 30px 16px 16px;
+  position: relative;
+  width: 360px;
+
+  ${props => ToastTypeVariations[props.type || 'info']}
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  > svg {
+    margin: 4px 12px 0px 0px;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      line-height: 20px;
+      opacity: 0.8;
+    }
+  }
+
+  button {
+    background: transparent;
+    border: 0px;
+    color: inherit;
+    opacity: 0.8;
+    position: absolute;
+    right: 16px;
+    top: 19px;
+  }
+
+  ${props =>
+    !props.$hasDescription &&
+    css`
+      align-items: center;
+
+      svg {
+        margin-top: 0px;
+      }
+    `}
+`;
