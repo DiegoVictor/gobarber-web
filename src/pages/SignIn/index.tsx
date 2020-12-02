f191e287482198e51e7a423b308e6b3457fab7b7
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-01 21:21:01 -0300

    Update signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,94 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
+import Logo from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um email válido')
+            .required('Email obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        } else {
+          addToast({
+            type: 'error',
+            title: 'Erro na autenticação',
+            description:
+              'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          });
+        }
+      }
+    },
+    [signIn, addToast],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={Logo} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu login</h1>
+
+            <Input icon={FiMail} name="email" type="text" placeholder="Email" />
+            <Input
+              icon={FiLock}
+              name="password"
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit">Entrar</Button>
+
+            <Link to="/forgot-password">Esqueci minha senha</Link>
+          </Form>
+
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,85 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import SignInBackground from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  align-items: stretch;
+  display: flex;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  max-width: 700px;
+  width: 100%;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  align-items: center;
+  animation: ${appearFromLeft} 1s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  form {
+    margin: 80px 0px;
+    text-align: center;
+    width: 340px;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    align-items: center;
+    color: #ff9000;
+    display: flex;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background-image: url(${SignInBackground});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
