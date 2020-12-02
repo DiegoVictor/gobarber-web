d456af40c1ff73460f8d99dc752c3113ff48185e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-02 18:44:24 -0300

    Remove default layout

--- src/pages/_layouts/default/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
-  height: 100%;
-  padding-bottom: 50px;
-`;
