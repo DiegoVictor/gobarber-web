4e8c3185ac48a1fe9990ef155b887f6d08edbac8
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-05 18:01:50 -0300

    Use new react api

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const element = document.getElementById('root');
+
+if (element) {
+  const root = ReactDOM.createRoot(element);
+  root.render(<App />);
+}
